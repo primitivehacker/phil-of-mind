ddb92aa6f9c63c99014bc6b21db8e98bcac1d1f2
siestaderek <siestaderek.com>
2017-12-09 23:39:15 -0700

    Get heroku working

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const app = require('./server/server');
 
-//app.listen(4000, () => {
-  //console.log('Listening');
-//});
-
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
+app.listen(4000, () => {
+  console.log('Listening');
 });
