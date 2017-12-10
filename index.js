07252e6a34c30438d6b5ac9f2fd1a5e752931763
siestaderek <siestaderek.com>
2017-12-09 23:23:26 -0700

    Heroku setup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = require('./server/server');
 
-app.listen(4000, () => {
-  console.log('Listening');
+//app.listen(4000, () => {
+  //console.log('Listening');
+//});
+
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
 });
